9bb8263e3d2e4e4be6af0e51c23867d222c02514
dpersaud <hithammer523@hotmail.com>
2023-03-28 18:26:22 -0400

    WIP - getting file names to be correct and everything on the same page

--- script1_featureizeData.py
+++ script1_featureizeData.py
@@ -2,8 +2,10 @@
 # IMPORT PACKAGES------------------------------------------------------------------------------------------------------
 """
 Created: Monday, February 20th, 2023
-Edited: Monday, March 13th, 2023
+Edited: Tuesday, March 28th, 2023
+
 Author: Daniel Persaud
+
 Description: This script is used to generate the elemental and magpie properties for the datasets used in
 the magpie reproducibility challenge and saves the dataframes to csv files for use in the next script.
 """
@@ -24,53 +26,70 @@ print('Packages imported successfully\n')
 dfOQMD = pd.read_csv('data/script1_in_oqmd_all.data', sep='  ', engine='python')
 # change the comp column name to strComposition
 dfOQMD = dfOQMD.rename(columns={'comp': 'strComposition'})
-# drop entiries with 'None' as the entry for bandgap
-#dfOQMD_bandgap = dfOQMD_bandgap.drop(dfOQMD_bandgap[dfOQMD_bandgap['bandgap'] == 'None'].index)
-# cast all entries in the 'bandgap' column to floats
-#dfOQMD_bandgap['bandgap'] = dfOQMD_bandgap['bandgap'].astype(float)
-#dfOQMD_bandgap = dfOQMD_bandgap.loc[dfOQMD_bandgap.groupby('comp')['bandgap'].idxmin()]
 print('OQMD data imported successfully')
 
 # use pandas to read bandgap.data
 dfBandgap = pd.read_csv('data/script1_in_bandgap.data', sep=' ', engine='python')
 # change the comp column name to strComposition
 dfBandgap = dfBandgap.rename(columns={'composition': 'strComposition'})
-# drop entiries with 'None' as the entry for bandgap
-#dfOQMD_bandgap = dfOQMD_bandgap.drop(dfOQMD_bandgap[dfOQMD_bandgap['bandgap'] == 'None'].index)
-# cast all entries in the 'bandgap' column to floats
-#dfOQMD_bandgap['bandgap'] = dfOQMD_bandgap['bandgap'].astype(float)
-#dfOQMD_bandgap = dfOQMD_bandgap.loc[dfOQMD_bandgap.groupby('comp')['bandgap'].idxmin()]
 print('Bandgap data imported successfully\n')
+
+# make a dataframe for the 'test' dataset - table 2 from the original paper
+dfTable2 = pd.DataFrame({'strComposition' : ['ScHg4Cl7',
+                                             'V2Hg3Cl7',
+                                             'Mn6CCl8',
+                                             'Hf4S11Cl2',
+                                             'VCu5Cl9'],
+                         'bandgap' : [1.26,
+                                      1.16,
+                                      1.28,
+                                      1.11,
+                                      1.19]})
+print('Test data imported created\n')
+                                           
 
 #%%
 # GENERATE THE COMPOSITION OBJECT FROM THE STRING----------------------------------------------------------------------
-# use the StrToComposition featurizer to convert the string composition to a composition object
+# use the StrToComposition featurizer to convert the string composition to a pymatgen.core.composition.Composition object
+
 dfOQMD = StrToComposition().featurize_dataframe(dfOQMD, "strComposition")
 print('Composition object generated successfully for entire OQMD dataset')
 
 dfBandgap = StrToComposition().featurize_dataframe(dfBandgap, "strComposition")
-print('Composition object generated successfully for entire Bandgap dataset\n')
+print('Composition object generated successfully for entire Bandgap dataset')
+
+dfTable2 = StrToComposition().featurize_dataframe(dfTable2, "strComposition")
+print('Composition object generated successfully for table 2\n')
 
 #%%
 # GENERATE THE ELEMENTAL FRACTIONS-------------------------------------------------------------------------------------
-# use the ElementFraction featurizer to generate the elemental fractions
+# use the ElementFraction featurizer to generate the elemental fractions based on the pymatgen.core.composition.Composition object
+
 dfOQMD = ElementFraction().featurize_dataframe(dfOQMD, col_id="composition", ignore_errors=True, return_errors=True)
 print('Elemental fractions generated successfully for entire OQMD dataset')
 
 dfBandgap = ElementFraction().featurize_dataframe(dfBandgap, col_id="composition", ignore_errors=True, return_errors=True)
-print('Elemental fractions generated successfully for entire Bandgap dataset\n')
+print('Elemental fractions generated successfully for entire Bandgap dataset')
+
+dfTable2 = ElementFraction().featurize_dataframe(dfTable2, col_id="composition", ignore_errors=True, return_errors=True)
+print('Elemental fractions generated successfully for table 2')
 #%%
 # GENERATE THE MAGPIE FEATURES-----------------------------------------------------------------------------------------
-# use element property to featurize the data using magpie data
+# use element property to generate the magpie features from the pymatgen.core.composition.Composition object
+
 dfOQMD = ElementProperty.from_preset("magpie").featurize_dataframe(dfOQMD, col_id="composition", ignore_errors=True, return_errors=True)
 print('Magpie features generated successfully for entire OQMD dataset')
 
 dfBandgap = ElementProperty.from_preset("magpie").featurize_dataframe(dfBandgap, col_id="composition", ignore_errors=True, return_errors=True)
 print('Magpie features generated successfully for entire Bandgap dataset\n')
 
+dfTable2 = ElementProperty.from_preset("magpie").featurize_dataframe(dfTable2, col_id="composition", ignore_errors=True, return_errors=True)
+print('Magpie features generated successfully for table 2\n')
+
 #%%
 # SAVE THE DATAFRAME TO A CSV FILE ------------------------------------------------------------------------------------
-# save the dataframe to a csv file
+
 dfOQMD.to_csv('data/script1_out_oqmd_all_featurized.csv', index=False)
 dfBandgap.to_csv('data/script1_out_bandgap_featurized.csv', index=False)
+dfTable2.to_csv('data/script1_out_table2_featurized.csv', index=False)
 print('Data saved successfully')
--- script2_dataCleaning.py
+++ script2_dataCleaning.py
@@ -2,16 +2,16 @@
 # IMPORT DEPENDENCIES--------------------------------------------------------------------------------------------------
 """
 Created: Monday, February 20th, 2023
-Edited: Monday, March 17th, 2023
-Author: Daniel Persaud
-Description: This script cleans the featurized dataset and produces 2 csv's which will be useful for the next script:
- - 
+Edited: Tuesday, March 28th, 2023
 
-and outputs a subset of the data which:
-- takes the lowest-energy compound for each unique composition
-- has a value for bandgap
-- has a bandgap energy between 0eV/atom and 1.5eV/atom
-- is a halogen
+Author: Daniel Persaud
+
+Description: This script cleans the featurized dataset and produces 2 csv's which will be used for the ML models:
+ -  the first is OQMD with duplicate compositions removed by taking the lowest-energy compound for each unique 
+    composition > this will be used for the 10-fold CV (originally used for model selection)
+ -  the second is the lowest-energy compound for each unique halogen with a bandgap value between 0eV/atom and
+    1.5eV/atom > this will be used to predict the the bandgap energies of the 5 compounds in table 5 of the original
+    paper
 """
 
 import numpy as np
@@ -20,8 +20,8 @@ import matplotlib.pyplot as plt
 
 #%%
 # IMPORT THE FEATUREIZED DATA------------------------------------------------------------------------------------------
-# read in the featurized data from script 1 (magpieReproducibilityChallange_1_dataFeaturization.py)
-dfOQMD_wFeatures_wElementFrac = pd.read_csv('data/oqmd_all_featurized.csv', low_memory=False)
+# read in the featurized data from script 1 (script1_out_oqmd_all_featurized.py)
+dfOQMD_wFeatures_wElementFrac = pd.read_csv('data/script1_out_oqmd_all_featurized.csv', low_memory=False)
 # replace 'None' string values with np.nan
 dfOQMD_wFeatures_wElementFrac.replace('None', np.nan, inplace=True)
 # make a list of all the columns that need to be cast to floats
@@ -33,8 +33,8 @@ lstFloatCols_oqmd.remove('ElementProperty Exceptions')
 # cast all the columns to floats
 dfOQMD_wFeatures_wElementFrac[lstFloatCols_oqmd] = dfOQMD_wFeatures_wElementFrac[lstFloatCols_oqmd].astype(float)
 
-# read in the featurized data from script 1 (magpieReproducibilityChallange_1_dataFeaturization.py)
-dfBandgap_wFeatures_wElementFrac = pd.read_csv('data/bandgap_featurized.csv', low_memory=False)
+# read in the featurized data from script 1 (script1_out_bandgap_featurized.py)
+dfBandgap_wFeatures_wElementFrac = pd.read_csv('data/script1_out_bandgap_featurized.csv', low_memory=False)
 # replace 'None' string values with np.nan
 dfBandgap_wFeatures_wElementFrac.replace('None', np.nan, inplace=True)
 # make a list of all the columns that need to be cast to floats
@@ -46,18 +46,38 @@ lstFloatCols_bandgap.remove('ElementProperty Exceptions')
 # cast all the columns to floats
 dfBandgap_wFeatures_wElementFrac[lstFloatCols_bandgap] = dfBandgap_wFeatures_wElementFrac[lstFloatCols_bandgap].astype(float)
 
+# read in the featurized data from script 1 (script1_out_table2_featurized.py)
+dfTable2_wFeatures_wElementFrac = pd.read_csv('data/script1_out_table2_featurized.csv', low_memory=False)
+# replace 'None' string values with np.nan
+dfTable2_wFeatures_wElementFrac.replace('None', np.nan, inplace=True)
+# make a list of all the columns that need to be cast to floats
+lstFloatCols_table2 = dfTable2_wFeatures_wElementFrac.columns.tolist()
+lstFloatCols_table2.remove('strComposition')
+lstFloatCols_table2.remove('composition')
+lstFloatCols_table2.remove('ElementFraction Exceptions')
+lstFloatCols_table2.remove('ElementProperty Exceptions')
+# cast all the columns to floats
+dfTable2_wFeatures_wElementFrac[lstFloatCols_table2] = dfTable2_wFeatures_wElementFrac[lstFloatCols_table2].astype(float)
 
-# OUTPUT: 
-#       dfOQMD_wFeatures_wElementFrac: pandas.DataFrame
-#           contains the featurized data for the OQMD dataset with all columns of the correct type
-#       dfBandgap_wFeatures_wElementFrac: pandas.DataFrame
-#           contains the featurized data for the bandgap dataset with all columns of the correct type
+# no further cleaning is needed for the table 2 dataset - save it
+dfTable2_wFeatures_wElementFrac.to_csv('data/script2_out_table2_clean.csv')
+
+
 #%%
 # CLEAN THE FEATURIZED DATA TO REMOVE DUPLICATES-----------------------------------------------------------------------
-# remove duplicate compositions by selecting the lowest energy compound for every duplicate
-dfOQMD_wFeatures_wElementFrac_clean = dfOQMD_wFeatures_wElementFrac.loc[dfOQMD_wFeatures_wElementFrac.groupby('strComposition')['energy_pa'].idxmin()]
+# remove duplicate compositions by selecting the lowest energy compound for every duplicate for the OQMD dataset
+dfOQMD_clean_modelSelection = dfOQMD_wFeatures_wElementFrac.loc[dfOQMD_wFeatures_wElementFrac.groupby('strComposition')['energy_pa'].idxmin()]
 # save the cleaned data to a csv file
-dfOQMD_wFeatures_wElementFrac_clean.to_csv('data/oqmd_all_featurized_clean.csv')
+dfOQMD_clean_modelSelection.to_csv('data/script2_out_oqmd_all_clean_modelSelection.csv')
 
-# remove rows that contains 
-# %%
+# remove duplicate compositions by selecting the lowest energy compound for every duplicate for the bandgap dataset
+dfBandgap_clean_framework= dfBandgap_wFeatures_wElementFrac.loc[dfBandgap_wFeatures_wElementFrac.groupby('strComposition')['energy_pa'].idxmin()]
+
+#%%
+# MAKE THE DATAFRAME FOR THE BANDGAP PREDICTION------------------------------------------------------------------------
+# select the halogens from OQMD
+dfOQMD_clean_framework = dfOQMD_clean_modelSelection[dfOQMD_clean_modelSelection['strComposition'].str.contains('F|Cl|Br|I')]
+# select the bandgap values between 0eV/atom and 1.5eV/atom
+dfOQMD_clean_framework = dfOQMD_clean_framework[(dfOQMD_clean_framework['bandgap_pa'] >= 0) & (dfOQMD_clean_framework['bandgap_pa'] <= 1.5)]
+# save the cleaned data to a csv file
+dfOQMD_clean_framework.to_csv('data/script2_out_oqmd_all_clean_framework.csv')
